a62e5d6507a635e4ed4b968414cfe7dff4d7b12d
Luan Nico <luanpotter27@gmail.com>
2017-11-11 23:03:17 -0200

    Better structure for dotfiles

--- functions.sh
+++ functions.sh
@@ -1,39 +1,71 @@
+# -- basics
 shopt -s globstar
+PS1='[\u@\h \W]\$ '
+# --
 
+# -- xrandr
 fix_res() {
   xrandr --newmode "1600x900_60.00" 118.25 1600 1696 1856 2112 900 903 908 934 -hsync +vsync
   xrandr --addmode VGA1 "1600x900_60.00"
   xrandr --output VGA1 --mode "1600x900_60.00"
 }
 
-alias src='source'
-alias chrome='google-chrome-beta'
-alias google-chrome='google-chrome-beta'
+house_monitor() {
+  xrandr --output HDMI1 --mode 1920x1080 --right-of LVDS1
+}
+# --
+
+# -- basic aliases
+alias ls='ls --color=auto'
+alias ll='ls -lA'
+alias lll='watch -n 1 ls -lrt'
 
 alias lock='xscreensaver-command -l'
 alias lockexit='pma exit && lock'
 
-alias ll='ls -lA --color=auto'
-alias lll='watch -n 1 ls -lrt'
-alias xc='xclip -selection c'
+alias git='hub'
 alias gcm='git commit -m '
 alias gall='git add -A'
+
+alias src='source'
 alias vbash='vim ~/.bashrc'
 alias sbash='src ~/.bashrc'
+
 alias mci='mvn clean install'
 alias mcint='mci -Dmaven.test.skip'
 
+alias chrome='google-chrome-beta'
+alias google-chrome='google-chrome-beta'
+
+alias xc='xclip -selection c'
+# --
+
+# -- advanced aliases
 alias c='cd'
 alias b='cd ..'
+alias s='git st'
+alias d='git diff -w'
 alias l='ll'
 alias t='tree'
 alias v='vim'
-alias s='git st'
-alias d='git diff'
+# --
 
-export PATH="/home/luan/softwares:$PATH"
+# -- path
+export PATH="$HOME/softwares/scripts:$HOME/softwares:$PATH"
 
-#locale
+flutter="$HOME/softwares/flutter/flutter/bin"
+if [ -d "$flutter" ]; then
+  export PATH="$flutter:$PATH"
+fi
+
+android="$HOME/softwares/android/Android"
+if [ -d "$android" ]; then
+  export ANDROID_HOME="$android"
+fi
+# --
+
+# -- locale
 export KEYMAP="br-abnt2.map.gz"
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
+# --
