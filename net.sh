f632a0dd820c6e45c2ab29bf3f75f1895a26861f
Luan Nico <luanpotter27@gmail.com>
2017-12-21 14:13:49 -0200

    dextra wifi

--- net.sh
+++ net.sh
@@ -3,4 +3,5 @@
 alias wired='sudo netctl start ethernet-dhcp'
 alias casa='sudo netctl start wlp2s0-Luan'
 alias eduroam='sudo netctl start wlp2s0-eduroam'
+alias dextra='sudo netctl start wlp2s0-DXT-CORP'
 alias netlist='sudo netctl list'
