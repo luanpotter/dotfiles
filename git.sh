f458142cf515ef0c36fe7bdf5f6237d458e52099
Luan Nico <luanpotter27@gmail.com>
2018-10-13 22:31:59 -0300

    more details about git config

--- git.sh
+++ git.sh
@@ -1,5 +1,7 @@
 # install git and hub, something like
 # sudo pacman -S git hub
+# dont forget to add your ssh key too; check:
+# https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
 
 git config --global user.email "luanpotter27@gmail.com"
 git config --global user.name "Luan Nico"
