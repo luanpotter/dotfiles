ffd719ee11a25d3cc44587eba897a19c47e91e35
Luan Nico <luanpotter27@gmail.com>
2021-10-30 13:32:46 -0400

    Update network names

--- net.sh
+++ net.sh
@@ -5,5 +5,5 @@ alias casa="sudo netctl start wlp3s0-Luan5G"
 alias house='sudo netctl start wlp3s0-Kitchener-H1'
 alias eduroam='sudo netctl start wlp3s0-eduroam'
 alias dextra='sudo netctl start wlp3s0-DXT-CORP'
-alias 4g='sudo netctl start "wlp3s0-AndroidAP"'
+alias 4g="sudo netctl start \"wlp3s0-Luan's Android\""
 alias netlist='sudo netctl list'
